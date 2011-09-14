2ad74a7c496cb16b68c5f2975fb3384cbf078788
tdh <tdh@e7da1e16-3a7a-0410-ad0b-fff4ac1689a5>
2011-09-14 20:35:09 +0000

    Updated client test class.
    git-svn-id: http://cdcvs.fnal.gov/subversion/privilege/branches/2.6.1@13 e7da1e16-3a7a-0410-ad0b-fff4ac1689a5

--- src/main/java/org/opensciencegrid/authz/xacml/client/XACMLClientTest.java
+++ src/main/java/org/opensciencegrid/authz/xacml/client/XACMLClientTest.java
@@ -1,13 +1,22 @@
 package org.opensciencegrid.authz.xacml.client;
 
 import org.opensaml.DefaultBootstrap;
+import org.opensaml.xacml.ctx.ResourceType;
+import org.opensaml.xacml.ctx.ActionType;
+import org.opensaml.xacml.ctx.EnvironmentType;
+import org.opensaml.xacml.ctx.SubjectType;
+import org.opensaml.xacml.ctx.DecisionType;
+import org.opensaml.xacml.ctx.ResultType;
+import org.opensaml.xacml.ctx.StatusType;
 import org.opensaml.xml.XMLObjectBuilderFactory;
 import org.opensaml.xacml.profile.saml.XACMLAuthzDecisionStatementType;
-import org.opensaml.xacml.ctx.*;
+
 import org.opensaml.Configuration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.xml.security.Init;
+
+import org.opensciencegrid.authz.xacml.common.XACMLConstants;
 import org.opensciencegrid.authz.xacml.stubs.Response;
 
 import java.util.LinkedHashMap;
@@ -19,49 +28,65 @@ public class XACMLClientTest
 
     public static void main(String[] args)
     {
-        String X509Subject = System.getenv(args[0].substring(1));
-        String condorName = System.getenv(args[1].substring(1));
-        String X509Issuer = System.getenv(args[2].substring(1));
-        String VO = System.getenv(args[3].substring(1));
-        String VOMSSigningSubject = System.getenv(args[4].substring(1));
-        String VOMSSigningIssuer = System.getenv(args[5].substring(1));
-        String VOMSFQAN = System.getenv(args[6].substring(1));
-        String VOMSPrimaryFQAN = System.getenv(args[7].substring(1));
-        String resourceType = System.getenv(args[8].substring(1));
-        String resourceDNSName = System.getenv(args[9].substring(1));
-        String resourceX509Name = System.getenv(args[10].substring(1));
-        String resourceX509Issuer = System.getenv(args[11].substring(1));
-        String authzServiceUrlStr = System.getenv(args[12].substring(1));
-
         XACMLClient client = new XACMLClient();
         String issuer = null;
 
-        LinkedHashMap<String,String> subjectAttribs = new LinkedHashMap<String,String>();
+        String authzServiceUrlStr = System.getenv("AUTHZ_SERVICE_URL");
 
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/subject-x509-id", X509Subject);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/subject-condor-canonical-name-id", condorName);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/subject-x509-issuer", X509Issuer);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/vo", VO);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/voms-signing-subject", VOMSSigningSubject);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/voms-signing-issuer", VOMSSigningIssuer);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/voms-fqan", VOMSFQAN);
-        subjectAttribs.put("http://authz-interop.org/xacml/subject/voms-primary-fqan", VOMSPrimaryFQAN);
+        LinkedHashMap<String,String> mySubjectAttribs = new LinkedHashMap<String,String>();
 
-        LinkedHashMap<String,String> resourceAttribs = new LinkedHashMap<String,String>();
-        resourceAttribs.put("urn:oasis:names:tc:xacml:1.0:resource:resource-id", resourceType);
-        resourceAttribs.put("http://authz-interop.org/xacml/resource/dns-host-name", resourceDNSName);
-        resourceAttribs.put("http://authz-interop.org/xacml/resource/resource-x509-id", resourceX509Name);
-        resourceAttribs.put("http://authz-interop.org/xacml/resource/resource-x509-issuer", resourceX509Issuer);
+        mySubjectAttribs.put(XACMLConstants.SUBJECT_X509_ID, System.getenv("SUBJECT_X509"));
+        mySubjectAttribs.put(XACMLConstants.SUBJECT_CONDOR_CANONICAL_NAME_ID, System.getenv("SUBJECT_CONDOR_CANONICAL_NAME"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_X509_ISSUER, System.getenv("SUBJECT_X509_ISSUER"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_VO_ID, System.getenv("SUBJECT_VO"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_VOMS_SIGNING_SUBJECT_ID, System.getenv("SUBJECT_VOMS_SIGNING_SUBJECT"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_VOMS_SIGNING_ISSUER_ID, System.getenv("SUBJECT_VOMS_SIGNING_ISSUER"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_VOMS_PRIMARY_FQAN_ID, System.getenv("SUBJECT_VOMS_PRIMARY_FQAN"));
+	    //SUBJECT_VOMS_FQAN_ID is added in the client code
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_CERTIFICATE_SERIAL_NUMBER_ID, System.getenv("SUBJECT_CERTIFICATE_SERIAL_NUMBER"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_CERTIFICATE_NOT_BEFORE_ID, System.getenv("SUBJECT_CERTIFICATE_NOT_BEFORE"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_CERTIFICATE_NOT_AFTER_ID, System.getenv("SUBJECT_CERTIFICATE_NOT_AFTER"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_CA_SERIAL_NUMBER_ID, System.getenv("SUBJECT_CA_SERIAL_NUMBER"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_VOMS_DNS_PORT_ID, System.getenv("SUBJECT_VOMS_DNS_PORT"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_CA_POLICY_OID_ID, System.getenv("SUBJECT_CA_POLICY_OID"));
+	    mySubjectAttribs.put(XACMLConstants.SUBJECT_CERT_CHAIN_ID, System.getenv("SUBJECT_CERT_CHAIN"));
 
-        LinkedHashMap<String,String> actionAttribs = new LinkedHashMap<String,String>();
-        actionAttribs.put("urn:oasis:names:tc:xacml:1.0:action:action-id", "http://authz-interop.org/xacml/action/action-type/access");
+        LinkedHashMap<String,String> myResourceAttribs = new LinkedHashMap<String,String>();
+        if("CE".equals(System.getenv("RESOURCE"))) {
+            myResourceAttribs.put(XACMLConstants.RESOURCE_ID, XACMLConstants.RESOURCE_CE);
+        } else if("WN".equals(System.getenv("RESOURCE"))) {
+            myResourceAttribs.put(XACMLConstants.RESOURCE_ID, XACMLConstants.RESOURCE_WN);
+        } else if("SE".equals(System.getenv("RESOURCE"))) {
+            myResourceAttribs.put(XACMLConstants.RESOURCE_ID, XACMLConstants.RESOURCE_SE);
+        }
+        myResourceAttribs.put(XACMLConstants.RESOURCE_DNS_HOST_NAME_ID, System.getenv("RESOURCE_DNS_HOST_NAME"));
+        myResourceAttribs.put(XACMLConstants.RESOURCE_X509_ID, System.getenv("RESOURCE_X509"));
+        myResourceAttribs.put(XACMLConstants.RESOURCE_X509_ISSUER_ID, System.getenv("RESOURCE_X509_ISSUER"));
 
-        for (int i=0; i<3; i++) {
+        LinkedHashMap<String,String> myActionAttribs = new LinkedHashMap<String,String>();
+        myActionAttribs.put(XACMLConstants.ACTION_ID, "http://authz-interop.org/xacml/action/action-type/access");
+        myActionAttribs.put(XACMLConstants.ACTION_RSL_STRING, System.getenv("ACTION_RSL_STRING"));
 
-            SubjectType subject = XACMLClient.getSubjectType(subjectAttribs, issuer);
-            ResourceType resource = XACMLClient.getResourceType(resourceAttribs, issuer);
-            ActionType action = XACMLClient.getActionType(actionAttribs, issuer);
-            EnvironmentType env = XACMLClient.getEnvironmentType();
+        for (int i=0; i<1; i++) {
+
+        SubjectType subject = XACMLClient.getSubjectType(mySubjectAttribs, issuer);
+        //Add FQANS
+        String fqan = System.getenv("SUBJECT_VOMS_PRIMARY_FQAN");
+        if(fqan!=null) {
+            XACMLClient.addSubjectFQAN(fqan, subject, issuer);
+        }
+        String secondary_fqan = System.getenv("SUBJECT_VOMS_SECONDARY_FQAN");
+        if(secondary_fqan!=null) {
+            XACMLClient.addSubjectFQAN(secondary_fqan, subject, issuer);
+        }
+        String tertiary_fqan = System.getenv("SUBJECT_VOMS_TERTIARY_FQAN");
+        if(tertiary_fqan!=null) {
+            XACMLClient.addSubjectFQAN(tertiary_fqan, subject, issuer);
+        }
+
+        ResourceType resource = XACMLClient.getResourceType(myResourceAttribs, issuer);
+        ActionType action = XACMLClient.getActionType(myActionAttribs, issuer);
+        EnvironmentType env = XACMLClient.getEnvironmentType();
 
             logger.info("Trying " + authzServiceUrlStr);
 
